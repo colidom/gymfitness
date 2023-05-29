13818273aa15c83e2c8f5dfb67260fc71bd895ff
colidom <colidom@outlook.com>
2023-05-29 01:31:17 +0100

    Registrando una Hoja de Estilos

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
 function extremegymfitness_menu()
 {
     register_nav_menus(array(
@@ -8,3 +12,11 @@ function extremegymfitness_menu()
 }
 
 add_action('init', 'extremegymfitness_menu');
+
+
+function extremegymfitness_scripts_styles()
+{
+    wp_enqueue_style('extremegymfitness-style', get_stylesheet_uri(), array(), '1.0.0');
+}
+
+add_action('wp_enqueue_scripts', 'extremegymfitness_scripts_styles');
