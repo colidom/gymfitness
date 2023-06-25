8cda637d1af2b51439e495f6e7e102bbe9644fb0
colidom <colidom@outlook.com>
2023-06-25 22:57:55 +0100

    Renombrado tema

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# extremegymfitness
+# gymxtremes
--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-function extremegymfitness_setup()
+function gymxtreme_setup()
 {
     // Imágenes destacadas
     add_theme_support('post-thumbnails');
@@ -13,22 +13,22 @@ function extremegymfitness_setup()
     add_theme_support('title-tag');
 }
 
-add_action('after_setup_theme', 'extremegymfitness_setup');
+add_action('after_setup_theme', 'gymxtreme_setup');
 
-function extremegymfitness_menu()
+function gymxtreme_menu()
 {
     register_nav_menus(array(
-        'main-manu' => __('Main Menu', 'extremegymfitness')
+        'main-manu' => __('Main Menu', 'gymxtreme')
     ));
 }
 
-add_action('init', 'extremegymfitness_menu');
+add_action('init', 'gymxtreme_menu');
 
 
-function extremegymfitness_scripts_styles()
+function gymxtreme_scripts_styles()
 {
     wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
     wp_enqueue_style('styles', get_stylesheet_uri(), array('normalize'), '1.0.0');
 }
 
-add_action('wp_enqueue_scripts', 'extremegymfitness_scripts_styles');
+add_action('wp_enqueue_scripts', 'gymxtreme_scripts_styles');
