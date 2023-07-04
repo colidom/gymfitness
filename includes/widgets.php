b844d62b4a30d35c0f2084f4ec135bffac9be7cc
colidom <colidom@outlook.com>
2023-07-05 00:54:51 +0100

    Mostrar la Información en el Widget

--- includes/widgets.php
+++ includes/widgets.php
@@ -16,7 +16,42 @@ class Gymxtreme_Lessons_Widget extends WP_Widget
 
     public function widget($args, $instance)
     {
-        echo $instance['quantity'];
+        ?>
+        <ul class="lessons-sidebar">
+        <?php 
+            $args = array(
+                'post_type' => 'gymxtreme_lessons',
+                'posts_per_page' => $instance['quantity']
+            );
+
+            $lessons = new WP_Query($args);
+            while($lessons->have_posts()) {
+                $lessons->the_post();
+                ?>
+                    <li>
+                        <div class="image">
+                            <?php the_post_thumbnail('medium')?>
+                        </div>
+                        <div class="lesson-content">
+                            <a href="<?php the_permalink() ?>">
+                                <h3><?php the_title() ?></h3>
+                            </a>
+                            <?php
+                            $start_time = get_field('start_time');
+                            $end_time = get_field('end_time');
+                            ?>
+                            <p>
+                                <?php the_field('lessons_days'); ?> -
+                                <?php echo $start_time . " to " . $end_time; ?>
+                            </p>
+                        </div>
+                    </li>
+                <?php
+            }
+            wp_reset_postdata();
+        ?>
+        </ul>
+        <?php 
     }
 
     public function form($instance)
