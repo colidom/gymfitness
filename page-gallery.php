00f002548350c3c5255486af57e639c5aa32dc12
colidom <colidom@outlook.com>
2023-07-07 00:05:09 +0100

    añadido the_content en galeria

--- page-gallery.php
+++ page-gallery.php
@@ -8,6 +8,8 @@ get_header();
     <?php
     while (have_posts()) : the_post();
         the_title('<h1 class="text-center text-primary">', '</h1>');
+
+        the_content();
     endwhile;
     ?>
 </main>
