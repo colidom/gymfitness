ef8bed066e9157bc14dff09bc949cd9727cd643e
colidom <colidom@outlook.com>
2023-07-07 00:34:57 +0100

    Obteniendo las imágenes de la galería

--- page-gallery.php
+++ page-gallery.php
@@ -9,7 +9,29 @@ get_header();
     while (have_posts()) : the_post();
         the_title('<h1 class="text-center text-primary">', '</h1>');
 
-        the_content();
+        // Get the gallery
+        $gallery = get_post_gallery(get_the_ID(), false);
+
+        // Get the ids in array
+        $gallery_images_ID = explode(",", $gallery['ids']);
+    ?>
+        <ul class="images-gallery">
+            <?php
+            foreach ($gallery_images_ID as $id) {
+                $imageLarge = wp_get_attachment_image_src($id, 'large')[0];
+                $imageFull = wp_get_attachment_image_src($id, 'full')[0];
+            ?>
+                <li>
+                    <a href="<?php echo $imageFull; ?>">
+                        <img src="<?php echo $imageLarge; ?>" alt="gallery image" />
+                    </a>
+                </li>
+            <?php
+
+            }
+            ?>
+        </ul>
+    <?php
     endwhile;
     ?>
 </main>
