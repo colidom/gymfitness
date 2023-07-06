ecf918f2e85981377fd7cf25f2842fcd0b768abc
colidom <colidom@outlook.com>
2023-07-07 00:36:01 +0100

    Renombrando variables

--- page-gallery.php
+++ page-gallery.php
@@ -18,12 +18,12 @@ get_header();
         <ul class="images-gallery">
             <?php
             foreach ($gallery_images_ID as $id) {
-                $imageLarge = wp_get_attachment_image_src($id, 'large')[0];
-                $imageFull = wp_get_attachment_image_src($id, 'full')[0];
+                $image_large = wp_get_attachment_image_src($id, 'large')[0];
+                $image_full = wp_get_attachment_image_src($id, 'full')[0];
             ?>
                 <li>
-                    <a href="<?php echo $imageFull; ?>">
-                        <img src="<?php echo $imageLarge; ?>" alt="gallery image" />
+                    <a href="<?php echo $image_full; ?>">
+                        <img src="<?php echo $image_large; ?>" alt="gallery image" />
                     </a>
                 </li>
             <?php
