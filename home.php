39a3c640b0c0edbd06b56fe53c96ce75186f56fd
colidom <colidom@outlook.com>
2023-07-08 01:49:08 +0100

    Mostrando las Entradas del Blog

--- home.php
+++ home.php
@@ -2,15 +2,16 @@
 get_header();
 ?>
 <main class="section container">
-    <?php
-    while (have_posts()) : the_post();
-
-        the_title();
-        the_content();
-
-    endwhile;
-    ?>
+    <ul class="grid-list">
+        <?php
+        while (have_posts()) {
+            the_post();
+            get_template_part('template-parts/blog');
+        }
+        ?>
+    </ul>
 </main>
+
 <?php
 get_footer();
 ?>
