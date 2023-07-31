b6b0da0cefb4a1e759c157bc1b7337a4c821fd80
colidom <colidom@outlook.com>
2023-07-31 22:23:04 +0100

    Añadiendo una Animación al Texto del header

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+function gymXtremeWordpress() {
+    if (document.querySelector(".swiper")) {
+        const options = {
+            loop: true,
+            autoplay: {
+                delay: 3000,
+            },
+        };
+        new Swiper(".swiper", options);
+    }
+
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector(".ml2");
+
+    if (textWrapper) {
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+        anime
+            .timeline({ loop: true })
+            .add({
+                targets: ".ml2 .letter",
+                scale: [4, 1],
+                opacity: [0, 1],
+                translateZ: 0,
+                easing: "easeOutExpo",
+                duration: 950,
+                delay: (el, i) => 70 * i,
+            })
+            .add({
+                targets: ".ml2",
+                opacity: 0,
+                duration: 1000,
+                easing: "easeOutExpo",
+                delay: 1000,
+            });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", gymXtremeWordpress);
