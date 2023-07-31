adbae2e3c03483536976d0124242a6863f8e971e
colidom <colidom@outlook.com>
2023-07-31 23:53:06 +0100

    Toda la parte de comentarios

--- single.php
+++ single.php
@@ -5,6 +5,25 @@ get_header();
     <?php
     get_template_part('template-parts/post');
     ?>
+
+    <div class="comments">
+        <?php comment_form(); ?>
+        <h3 class="text-center text-primary comments">Comments</h3>
+        <ul class="comments-list">
+            <?php
+            $comments = get_comments(array(
+                'post_id' => $post->ID,
+                'status'  => 'approve'
+            ));
+
+            wp_list_comments(array(
+                'per_page' => 10,
+                'reverse_top_level' => false
+            ), $comments);
+            ?>
+        </ul>
+    </div>
+
 </main>
 <?php
 get_footer();
